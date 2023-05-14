2d2e51eef0a94fc48c0b44a6edf1ccab1455ab19
semika100 <133549797+semika100@users.noreply.github.com>
2023-05-14 07:37:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94716055947', 'SEMIKA', true],
+  ['94775988435', 'semika', true], 
   [''] 
 ] //Number of owners
 
